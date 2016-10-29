07602927442a80dc8fb07a81d33bcc459fe06827
Di Wu <diwu1989@users.noreply.github.com>
2016-10-28 22:35:28 -0700

    Update buildpack-run.sh

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
-URL="https://github.com/metabase/metabase-deploy/raw/v0.20.2/target/uberjar/metabase.jar"
+URL="https://github.com/metabase/metabase-deploy/raw/v0.20.3/target/uberjar/metabase.jar"
 wget --progress=bar $URL -O metabase.jar
