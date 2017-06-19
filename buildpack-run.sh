463c676535594b48ddcf4e0c5229b8ac2c9df8e7
Di Wu <dw323@cornell.edu>
2017-06-19 14:29:06 -0700

    upgrade metabase

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
-URL="https://github.com/metabase/metabase-deploy/raw/v0.20.3/target/uberjar/metabase.jar"
+URL="http://downloads.metabase.com/v0.24.2/metabase.jar"
 wget --progress=bar $URL -O metabase.jar
