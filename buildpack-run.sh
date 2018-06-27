ab1b2ddea2f82f78150ee61c22e055ac89bb5243
Marcus Saad <marcus@betterworks.com>
2018-06-27 12:22:52 -0700

    Updating metabase from 0.28.1 to 0.29.3

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
-URL="http://downloads.metabase.com/v0.28.1/metabase.jar"
+URL="http://downloads.metabase.com/v0.29.3/metabase.jar"
 wget --progress=bar $URL -O metabase.jar
